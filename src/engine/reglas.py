7f693fc07e8babd96cf284d16cc055ffe462e09f
Alex <alx5409@gmail.com>
2025-10-15 09:49:03 +0200

    Add reglas
    Declarando los métodos que tendrá la clase reglas

--- src/engine/reglas.py
+++ src/engine/reglas.py
@@ -1,4 +1,6 @@
-from piezas import Color, Rey, Pieza
+from array import array
+
+from piezas import Color, Rey, Pieza, Peon
 from tablero import Tablero
 
 class Reglas:
@@ -12,3 +14,25 @@ class Reglas:
 
         posicion_rey = rey.posicion_actual_entera
         piezas_oponentes = self.tablero.listar_piezas_por_color(Color.NEGRA if color == Color.BLANCA else Color.BLANCA)
+    
+    def es_jaque_mate(self, color: Color) -> bool:
+        pass
+
+    def es_ahogado(self, color: Color) -> bool:
+        pass
+
+    def es_tablas(self) -> bool:
+        pass
+
+    def es_movimiento_legal(self, pieza: Pieza, destino: array) -> bool:
+        pass
+        
+    def puede_enrocar(self, color: Color, lado:str) -> bool:
+        pass
+
+    def puede_capturar_al_paso(self, peon: Peon) -> bool:
+        pass
+    
+    def simular_movimiento(self, pieza: Pieza, destino: array) -> Tablero:
+        pass
+    
