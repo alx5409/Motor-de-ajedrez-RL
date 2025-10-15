942ee5cfd4f95cfc4910d4c5d4e7d2a168ce1fd0
Alex <alx5409@gmail.com>
2025-10-15 16:33:04 +0200

    Implementado el método para comprobar si un jugador esta ahogado

--- src/engine/reglas.py
+++ src/engine/reglas.py
@@ -9,7 +9,7 @@ class Reglas:
 
     def es_jaque(self, color: Color) -> bool:
         """
-        Devuelve True si el rey del color dado está en jaque.
+        Devuelve True si el rey del color dado está en jaque, , False en caso contrario.
         """
         rey: Rey = self.tablero.buscar_rey(color)
         if not rey:
@@ -24,10 +24,51 @@ class Reglas:
         return False
     
     def es_jaque_mate(self, color: Color) -> bool:
-        pass
-
+        """
+        Devuelve True si el rey del color está en jaque mate, False en caso contrario.
+        """
+        # Comprueba si el rey del color está en jaque.
+        if (not self.es_jaque()):
+            return False
+        
+        # Comprueba si todas las piezas del color tienen las casillas de los movimientos validos del rey en objetivo
+        piezas_propias = self.tablero.listar_piezas_por_color(color)
+        for pieza in piezas_propias:
+            # Prueba todos los movimientos posibles en el tablero
+            for fila in range(8):
+                for columna in range(8):
+                    destino = array('i', [fila, columna])
+                    # Si el movimiento es legal y tras simularlo el rey no está en jaque, no es jaque mate
+                    es_valido = pieza.comprobar_movimiento_valido(destino, self.tablero.matriz_piezas)
+                    if not es_valido:
+                        continue
+                    tablero_simulado = self.simular_movimiento(pieza, destino)
+                    reglas_simuladas = Reglas(tablero_simulado)
+                    if not reglas_simuladas.es_jaque(color):
+                        return False
+                    
+        return True
+        
     def es_ahogado(self, color: Color) -> bool:
-        pass
+        """
+        Devuelve True si el jugador del color dado está ahogado (no tiene movimientos legales y no está en jaque).
+        """
+        if self.es_jaque(color):
+            return False
+
+        piezas_propias = self.tablero.listar_piezas_por_color(color)
+        for pieza in piezas_propias:
+            for fila in range(8):
+                for columna in range(8):
+                    destino = array('i', [fila, columna])
+                    es_valido = pieza.comprobar_movimiento_valido(destino, self.tablero.matriz_piezas)
+                    if not es_valido:
+                        continue
+                    tablero_simulado = self.simular_movimiento(pieza, destino)
+                    reglas_simuladas = Reglas(tablero_simulado)
+                    if not reglas_simuladas.es_jaque(color):
+                        return False
+        return True
 
     def es_tablas(self) -> bool:
         pass
