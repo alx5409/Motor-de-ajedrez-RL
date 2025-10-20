6d6a66b1e1f862d23509b59e83e3f6fe6db970a9
Alex <alx5409@gmail.com>
2025-10-20 08:50:48 +0200

    Añadir declaración de métodos de Evaluador - Se han agregado las declaraciones de los métodos de la clase Evaluador para implementarlos

--- src/engine/evaluador.py
+++ src/engine/evaluador.py
@@ -5,3 +5,25 @@ from piezas import *
 class Evaluador:
 
     def __init__(self, tablero: Tablero, color_a_evaluar: Color = Color.BLANCA):
+        pass
+
+    def evaluar(self) -> float:
+        pass
+
+    def evaluar_mobilidad(self) -> float:
+        pass
+
+    def evaluar_estructura_peones(self) -> float:
+        pass
+
+    def evaluar_seguridad_rey(self) -> float:
+        pass
+
+    def evaluar_control_centro(self) -> float:
+        pass
+
+    def es_jaque_mate(self) -> bool:
+        pass
+
+    def es_tablas(self) -> bool:
+        pass
